2386e45e56313f0be8d84578eb0eb71ff71b73ca
dengmiao <1297320571@qq.com>
2019-07-01 09:39:20 +0800

    webflux 跨域

--- src/main/java/com/miao/boot/blog/BlogApplication.java
+++ src/main/java/com/miao/boot/blog/BlogApplication.java
@@ -5,7 +5,12 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
 /**
@@ -26,5 +31,26 @@ public class BlogApplication implements ApplicationRunner {
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 		log.info("启动完成。。");
+	}
+
+	private CorsConfiguration buildConfig() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.addAllowedOrigin("*");
+		corsConfiguration.addAllowedHeader("*");
+		corsConfiguration.addAllowedMethod("*");
+		corsConfiguration.addExposedHeader(HttpHeaders.SET_COOKIE);
+		return corsConfiguration;
+	}
+
+	/**
+	 * 跨域过滤器
+	 *
+	 * @return
+	 */
+	@Bean
+	public CorsWebFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", buildConfig());
+		return new CorsWebFilter(source);
 	}
 }
