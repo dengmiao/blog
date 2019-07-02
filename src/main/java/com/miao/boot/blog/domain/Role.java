d482802c79300b5e29292ac4410a4d806a49de18
dengmiao <1297320571@qq.com>
2019-07-02 17:16:14 +0800

    领域模型

--- src/main/java/com/miao/boot/blog/domain/Role.java
+++ src/main/java/com/miao/boot/blog/domain/Role.java
@@ -1,7 +1,11 @@
 package com.miao.boot.blog.domain;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
 
 /**
  * @title: Role
@@ -10,6 +14,8 @@ import org.springframework.data.mongodb.core.mapping.Document;
  * @create: 2019-06-11 14:18
  **/
 @Data
+@Accessors(chain = true)
+@NoArgsConstructor
 @Document(collection = "role")
 public class Role {
 
@@ -18,4 +24,9 @@ public class Role {
     private String code;
 
     private String name;
+
+    /**
+     * 权限id集合
+     */
+    private List<String> permissions;
 }
--- src/main/java/com/miao/boot/blog/domain/User.java
+++ src/main/java/com/miao/boot/blog/domain/User.java
@@ -4,9 +4,12 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.Set;
 
 /**
  * @title: User
@@ -32,4 +35,21 @@ public class User implements Serializable {
     private String email;
 
     private Integer status;
+
+    /**
+     * 角色id集合
+     */
+    private List<String> roles;
+
+    /**
+     * 用户角色集合
+     */
+    @Transient
+    private Set<Role> roleList;
+
+    /**
+     * 用户权限集合
+     */
+    @Transient
+    private List<Permission> permissionList;
 }
