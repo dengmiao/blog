03eb7798151a4a81548bf9f9cf1184e8b97898b7
dengmiao <1297320571@qq.com>
2019-07-02 17:15:44 +0800

    时间序列化

---
+++ src/main/java/com/miao/boot/blog/configuration/LocalDateTimeSerializerConfig.java
@@ -0,0 +1,33 @@
+package com.miao.boot.blog.configuration;
+
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @title: LocalDateTimeSerializerConfig
+ * @description: 全局日期序列化
+ * @author: dengmiao
+ * @create: 2019-07-02 08:47
+ **/
+@Configuration
+public class LocalDateTimeSerializerConfig {
+
+    @Value("${spring.jackson.date-format:yyyy-MM-dd HH:mm:ss}")
+    private String pattern;
+
+    @Bean
+    public LocalDateTimeSerializer localDateTimeDeserializer() {
+        return new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
+        return builder -> builder.serializerByType(LocalDateTime.class, localDateTimeDeserializer());
+    }
+}
